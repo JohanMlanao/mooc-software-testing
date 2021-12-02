08de051189636b6a99fe821487bd6e8187de5037
JohanMlanao <johan.mlanao@gmail.com>
2021-12-02 14:03:57 +0100

    Complete CaesarShiftCipherTest

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,18 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
 public class CaesarShiftCipherTest {
+
+    private final CaesarShiftCipher cs = new CaesarShiftCipher();
+
+    @Test
+    public void Shifty() {
+        Assertions.assertEquals("def", cs.CaesarShiftCipher("abc",3));
+        Assertions.assertEquals("abc", cs.CaesarShiftCipher("xyz",3));
+        Assertions.assertEquals("invalid", cs.CaesarShiftCipher("12345",2));
+    }
 
 }
